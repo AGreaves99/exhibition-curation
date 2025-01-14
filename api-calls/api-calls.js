7378438fb1f3205064454c82368c8c3d5b842e69
AGreaves99 <ajohngreaves@outlook.com>
2025-01-14 13:49:29 +0000

    feat: sorting in getArtworks call

--- api-calls/api-calls.js
+++ api-calls/api-calls.js
@@ -4,14 +4,28 @@ const artic = axios.create({
   baseURL: "https://api.artic.edu/api/v1",
 });
 
-export function getArtworks(limit = "10", search = "") {
+export function getArtworks(limit = "10", search = null, sort = null) {
+  const sortOptions = {
+    // The API ascending and descending sort options are reversed
+    "artist-desc": ["artist_title.keyword", "asc"],
+    "artist-asc": ["artist_title.keyword", "desc"],
+    "title-desc": ["title.keyword", "asc"],
+    "title-asc": ["title.keyword", "desc"],
+    "date-asc": ["date_end", "desc"],
+    "date-desc": ["date_end", "asc"],
+  };
   return artic
     .get("/artworks/search", {
       params: {
-        "query[term][is_public_domain]": true,
-        fields: "id,title,image_id,artist_title,thumbnail.alt_text",
+        query: {
+          term: { is_public_domain: true },
+        },
+        fields: "id,title,image_id,artist_title,thumbnail.alt_text,date_end",
         size: limit,
         q: search,
+        sort: sortOptions[sort] && {
+          [sortOptions[sort][0]]: sortOptions[sort][1],
+        },
       },
     })
     .then((response) => response.data)
