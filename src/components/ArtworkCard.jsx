59b1b4db3f5eb4c770c6f5ef261b0da039189cd2
AGreaves99 <ajohngreaves@outlook.com>
2025-01-17 14:17:39 +0000

    feat: update component structure and styling to include children

--- src/components/ArtworkCard.jsx
+++ src/components/ArtworkCard.jsx
@@ -7,18 +7,22 @@ export function ArtworkCard({
   image_id,
   alt_text,
   iiif_url,
+  children,
 }) {
   return (
-    <a href={`/artic/${id}`} class="artwork-card">
-      <img
-        src={image_id && `${iiif_url}/${image_id}/full/400,/0/default.jpg`}
-        alt={alt_text}
-        class="artwork-image"
-      />
-      <div class="artwork-details">
-        <h1 class="artwork-title">{title}</h1>
-        <p class="artwork-artist">{artist}</p>
-      </div>
-    </a>
+    <div className="artwork-card">
+      <a href={`/artic/${id}`} className="artwork-link">
+        <img
+          src={image_id && `${iiif_url}/${image_id}/full/400,/0/default.jpg`}
+          alt={alt_text}
+          className="artwork-image"
+        />
+        <div className="artwork-details">
+          <h1 className="artwork-title">{title}</h1>
+          <p className="artwork-artist">{artist}</p>
+        </div>
+      </a>
+      <div className="artwork-children">{children}</div>
+    </div>
   );
 }
