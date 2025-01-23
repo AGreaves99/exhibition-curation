487fb09061c6e2a4a713feb92c194272be2da421
AGreaves99 <ajohngreaves@outlook.com>
2025-01-23 14:28:51 +0000

    feat: use ArtworkDetailsSkeleton as fallback in SingleArtwork component

--- src/pages/SingleArtwork.jsx
+++ src/pages/SingleArtwork.jsx
@@ -1,12 +1,13 @@
 import { lazy, Suspense } from "preact/compat";
 import "../styles/singleArtwork.css";
+import { ArtworkDetailsSkeleton } from "../components/loading-states/ArtworkDetailsSkeleton";
 const SingleArtworkDetails = lazy(() =>
   import("../components/SingleArtworkDetails")
 );
 
 export const SingleArtwork = () => {
   return (
-    <Suspense fallback={<p>Loading artwork</p>}>
+    <Suspense fallback={<ArtworkDetailsSkeleton />}>
       <SingleArtworkDetails />
     </Suspense>
   );
